fe332016e425ad63c51a0e26bda18aebf5c2d924
Ryan Smith <ryansmith05@gmail.com>
2021-05-25 20:50:33 -0700

    add search for tags

--- scripts/card-script.js
+++ scripts/card-script.js
@@ -1,7 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     addEnterSubmitListener();
     getCardsFromStorage();
+    addSearchListener();
 });
+
+function addSearchListener() {
+    var search = document.getElementById('search')
+
+    search.addEventListener('keyup', function(e) {
+        if (e.key == 'Enter') {
+            if (this.value === '') {
+                Array.from(document.getElementById('cards').children).forEach(card => card.remove());
+
+                getCardsFromStorage();
+            } else {
+                var cards = JSON.parse(localStorage.getItem('card-storage'));
+                Array.from(document.getElementById('cards').children).forEach(card => card.remove());
+
+                cards
+                    .filter(card => card.tags)
+                    .filter(card => card.tags.includes(this.value))
+                    .forEach(card => buildCardNodes(card.id, card.name, card.content, card.imageUrl, card.tags))
+            }
+        }
+
+    });
+}
 
 function addEnterSubmitListener() {
     document.getElementById('content').addEventListener('keyup', function(e) {
